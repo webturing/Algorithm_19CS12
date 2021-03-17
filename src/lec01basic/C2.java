90af595a87112e9e5de3d50e7d34121a4b46e8d7
webturing <zj@webturing.com>
2021-03-17 16:48:02 +0800

    week02b 归并排序的实现

--- src/lec01basic/C2.java
+++ src/lec01basic/C2.java
@@ -7,7 +7,7 @@ public class C2 {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         BigInteger n = cin.nextBigInteger();
-        BigInteger s = n.multiply(n.add(BigInteger.ONE)).divide(BigInteger.TWO);//s=n*(n+1)/2
+        BigInteger s = n.multiply(n.add(BigInteger.ONE)).divide(new BigInteger("2"));//s=n*(n+1)/2
         System.out.println(s);
         System.out.println(s.isProbablePrime(100));//0.25^100
     }
